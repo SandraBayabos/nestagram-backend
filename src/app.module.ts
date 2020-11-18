42496df89b3a94084e5dd7e0a9808afe6e1dce6d
sandra <sandra.bayabos@gmail.com>
2020-11-18 15:26:45 +0800

    first commit => created user files

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://localhost/nestagram-backend',
+       { useNewUrlParser: true }
+    )
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}

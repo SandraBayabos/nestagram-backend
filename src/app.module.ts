e9e0d2551e4d45e4bb2b81fab3685ae79a0fad64
sandra <sandra.bayabos@gmail.com>
2020-11-18 16:18:28 +0800

    created users moduler & users schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb://localhost/nestagram-backend',
        { useNewUrlParser: true }
-    )
+    ),
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,38 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+/*
+@Schema() decorator marks a class as a schema definition. Maps User class to MongoDB collection
+of the same name 
+@Prop() defines a property in the document
+*/
+
+@Schema()
+export class User {
+  @Prop()
+  name: string
+
+  @Prop()
+  email: string
+
+  @Prop()
+  password: string
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+/*
+
+Without decorators
+
+import * as mongoose from 'mongoose'
+
+export const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+})
+
+*/

0fa9b3bcde1606abe16ad8b2a4ce08fe3e0aa069
sandra <sandra.bayabos@gmail.com>
2020-12-01 14:31:10 +0800

    edited images.service

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ImagesController } from './images.controller';
 import { ImagesService } from './images.service';
+import { ImageSchema } from './schemas/image.schema';
 
 @Module({
+  imports: [
+    // making the Image schema injectable
+    MongooseModule.forFeature([{name: 'Image', schema: ImageSchema}])
+  ],
   controllers: [ImagesController],
   providers: [ImagesService]
 })
--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Image } from './interfaces/image.interface'
 
 @Injectable()
-export class ImagesService {}
+export class ImagesService {
+  constructor(@InjectModel('Image') private readonly imageModel: Model<Image>) {}
+
+  
+}
